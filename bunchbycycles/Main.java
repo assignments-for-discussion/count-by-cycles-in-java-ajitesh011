db5b8c717a18abf2c0378173debf0209ca0513a6
Ajitesh Narayan Bajpayee <ajiteshnarayanb@gmail.com>
2021-12-21 13:17:28 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,6 +9,29 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+     int c=0;
+    int b=0;
+    int d=0;
+    for (int i=0; i<cycles.length;i++)
+    {
+        if(cycles[i]<150)
+        {
+            c++;
+        }
+        else if(cycles[i]>150 && cycles[i]<=649)
+        {
+            b++;
+        }
+        else
+        {
+            d++;
+        }
+        
+        
+    }
+    counts.lowCount=c;
+    counts.mediumCount=b;
+    counts.highCount=d;
     return counts;
   }
 
@@ -19,6 +42,9 @@ public class Main {
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 2);
     System.out.println("Done counting :)\n");
+     System.out.println("No. of low are",counts.lowCount);
+    System.out.println("No. of medium are",counts.mediumCount);
+    System.out.println("No. of high are",counts.highCount);
   }
 
   public static void main(String[] args) {
